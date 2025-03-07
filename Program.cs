678f0633a595e02fd2853b7f1a7447b8d0c144f3
Behnam <m.behnam.4929@gmail.com>
2025-03-07 20:24:00 +0330

    Done with Delete_task

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.Data.Sqlite;
 
 namespace TD
@@ -12,16 +13,31 @@ namespace TD
             Console.Clear();
 
             Connect_To_db();
-            Menu();
-
-            Console.Read();
+            while(true)
+            {
+                Menu();
+            }
         }
         public static void Menu()
         {
             Console.WriteLine("\n\t\t\t\t\t\t*** Todo List ***");
-            Console.WriteLine("\n\t\t\t\t\t\t[1] Create Task\n\t\t\t\t\t\t[2] Delete Task\n\n\n\t\t\t");
-            Console.WriteLine( "⚫️"+ DateTime.Today.ToShortDateString());
+            Console.WriteLine("\n\t\t\t\t\t\t[1] Create Task\t\t\t "+ "⚫️"+ DateTime.Today.ToShortDateString()+ "\n\t\t\t\t\t\t[2] Delete Task\n\n\n\t\t\t");
             Print_Tasks();
+            while(true)
+            {
+                var key = Console.ReadKey(true);
+                switch(key.Key)
+                {
+                    case ConsoleKey.D1:
+                        Create_Task();
+                        return;
+                    case ConsoleKey.D2:
+                        Delete_Task();
+                        return;
+                    default:
+                        break; 
+                }
+            }
         }
 
         public static void Connect_To_db()
@@ -39,7 +55,7 @@ namespace TD
             }
         }
         public static void Print_Tasks()
-            {
+        {
             using(var connection = new SqliteConnection("Data Source=tasks.db"))
             {
                 connection.Open();
@@ -52,7 +68,7 @@ namespace TD
                         var title = reader["title"].ToString();
                         var date = reader["date"].ToString();
                         var done = reader["done"];
-                        Console.WriteLine(title + "\t" + date + "\n\n\t\t\t");
+                        Console.WriteLine("\n\n\t\t\t\t" + "⚫️" + title + "\t" + "Date: "+ date + "  🔲");
                     }
                 }
                 command.ExecuteNonQuery(); 
@@ -108,9 +124,108 @@ namespace TD
                 command.Parameters.AddWithValue("@title", title);
                 command.Parameters.AddWithValue("@date", date);
                 command.Parameters.AddWithValue("@done", false);
+                command.ExecuteNonQuery();
             }
             Console.Clear();
-            Menu();  
+            return;
+        }
+
+        public static void Delete_Task()
+        {
+            Console.Clear();
+            Console.WriteLine("\n\t\t\t\tSelect the task and press Enter to delete. Backspace to return");
+            using(var connection = new SqliteConnection("Data Source=tasks.db"))
+            {   
+                int count = 0;
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM tasks";
+                using(var reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        count ++ ;
+                        var title = reader["title"].ToString();
+                        var date = reader["date"].ToString();
+                        var done = reader["done"];
+                        Console.WriteLine("\n\n\t\t\t\t" + "⚫️" + title + "\t" + "Date: "+ date);
+                    }
+                }
+                if (count == 0)
+                {
+                    Console.WriteLine("\t\t\tYou have no tasks yet :) Create a task by pressing 1 in the main menu");
+                    Thread.Sleep(3000);
+                    return;
+                }
+                else
+                {
+                    int row = 4;
+                    Console.SetCursorPosition(26, row);
+                    Console.Write("➡️");
+                    while(true)
+                    {
+                        var key = Console.ReadKey(true);
+                        switch(key.Key)
+                        {
+                            case ConsoleKey.UpArrow:
+                                if (row != 4)
+                                {
+                                    Console.SetCursorPosition(26, row); // Delete the current arrow
+                                    Console.Write(" ");                 //
+                                    row = row-3;
+                                    Console.SetCursorPosition(26, row); // Set new arrow
+                                    Console.Write("➡️");
+                                }
+                                break;
+                            case ConsoleKey.DownArrow:
+                                if (row != (1+(count))*3)
+                                {
+                                    Console.SetCursorPosition(26, row); // Delete the current arrow
+                                    Console.Write(" ");                 //
+                                    row = row+3;
+                                    Console.SetCursorPosition(26, row); // Set new arrow
+                                    Console.Write("➡️");
+                                }
+                                break;
+                            case ConsoleKey.Enter:
+                                Query_Delete((row-1)/3);
+                                // Query_Delete(1);
+                                Console.Clear();
+                                return;
+                            case ConsoleKey.Backspace:
+                                Console.Clear();
+                                return;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        public static void Query_Delete(int index)
+        {
+            using(var connection = new SqliteConnection("Data Source=tasks.db"))
+            {   
+                int count = 0;
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM tasks";
+                using(var reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        count ++ ;
+                        if (count==index)
+                        {
+                            var del_command = connection.CreateCommand();
+                            del_command.CommandText = "DELETE FROM tasks WHERE id=@id";
+                            del_command.Parameters.AddWithValue("@id", reader["id"]);
+                            del_command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
         }
     }
 }
